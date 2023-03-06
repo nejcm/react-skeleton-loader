115ea2021ba24e9df96758639b8484fcebcbb0aa
nejcm <nmursi2@gmail.com>
2023-03-06 07:13:23 +0000

    Deploying to gh-pages from  @ 7755ebb9cadd74094f9ea338728812f76b5a5230 🚀

---
+++ client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/home/runner/work/react-skeleton-loader/react-skeleton-loader/node_modules/infima/dist/css/default/default.css'),
+  require('/home/runner/work/react-skeleton-loader/react-skeleton-loader/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/home/runner/work/react-skeleton-loader/react-skeleton-loader/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/home/runner/work/react-skeleton-loader/react-skeleton-loader/packages/docs/src/css/custom.css'),
+];
--- styles-3959adbb69757b17e30b.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[15],[]]);
-//# sourceMappingURL=styles-3959adbb69757b17e30b.js.map
